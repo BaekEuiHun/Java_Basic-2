533449496e5f98be71f537aa899b0748ad9f35a3
BaekEuiHun <euihunbaek@gmail.com>
2024-06-07 15:34:15 +0900

    update

--- src/class1/classStart5.java
+++ src/class1/classStart5.java
@@ -5,20 +5,21 @@ import ex.ProductOrder;
 public class classStart5 {
     public static void main(String[] args) {
 
-        Student students[] = new Student[2];
 
         Student student1 = new Student();
         student1.name = "학생1";
         student1.age = 18;
         student1.grade = 90;
-        students[0] = student1;
+
 
         Student student2 = new Student();
         student2.name = "학생2";
         student2.age = 19;
         student2.grade = 80;
-        students[1] = student1;
 
+        Student students[] = new Student[2];
+        students[0] = student1;
+        students[1] = student2;
 
 
         for (int i = 0; i < students.length; i++) {
