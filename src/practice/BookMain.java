9fff762e09e650867bb2a61fb20e32364afb7b5f
BaekEuiHun <euihunbaek@gmail.com>
2024-06-13 18:22:53 +0900

    update

---
+++ src/practice/BookMain.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class BookMain {
+    public static void main(String[] args) {
+        Book book1 = new Book();
+        book1.displayInfo();
+
+        Book book2 = new Book("Hello Java", "Seo");
+        book2.displayInfo();
+
+        Book book3 = new Book("JPA", "kim", 300);
+        book3.displayInfo();
+    }
+}
