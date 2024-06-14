a4b2b2e214eaa9ec9cfd434fcc286b633b1406cf
BaekEuiHun <euihunbaek@gmail.com>
2024-06-14 18:56:44 +0900

    update

---
+++ src/access/CounterMain.java
@@ -0,0 +1,15 @@
+package access;
+
+import ex2.MaxCounter;
+
+public class CounterMain {
+    public static void main(String[] args) {
+        MaxCounter counter = new MaxCounter(4);
+        counter.increment();
+        counter.increment();
+        counter.increment();
+        counter.increment();
+        int count = counter.getCount();
+        System.out.println(count);
+    }
+}
--- src/access/SpeakerMain.java
+++ src/access/SpeakerMain.java
@@ -1,6 +1,6 @@
 package access;
 
-public class  SpeakerMain {
+public class SpeakerMain {
     public static void main(String[] args) {
         Speaker speaker = new Speaker(90);
         speaker.showVolume();
