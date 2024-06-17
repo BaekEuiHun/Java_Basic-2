69ac8ee51db190650788eaa3098938d3f5f72a78
BaekEuiHun <euihunbaek@gmail.com>
2024-06-17 22:05:19 +0900

    Update classStart2.java

--- src/class1/classStart2.java
+++ src/class1/classStart2.java
@@ -1,7 +1,7 @@
 package class1;
 
 public class classStart2 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
         String[] studentNames = {"학생1", "학생2", "학생3"};
         int[] studentAges = {15,  16, 17};
