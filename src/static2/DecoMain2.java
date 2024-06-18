23c4110a3d38da833a28aa5cc50c1ec32ae9f921
BaekEuiHun <euihunbaek@gmail.com>
2024-06-18 17:19:57 +0900

    update

--- src/static2/DecoMain2.java
+++ src/static2/DecoMain2.java
@@ -1,12 +1,10 @@
 package static2;
 
-import java.security.spec.RSAOtherPrimeInfo;
-
-public class DecoMain1 {
+public class DecoMain2 {
     public static void main(String[] args) {
         String s = "hello java";
-        DecoUtil1 utils = new DecoUtil1();
-        String deco = utils.deco(s);
+        String deco = DecoUtil2.deco(s);
+
         System.out.println("before: " + s);
         System.out.println("after: " + deco);
 
