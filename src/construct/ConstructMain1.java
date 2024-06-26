1af783cc9b26907767ebe93e65fed7e52fd9f002
BaekEuiHun <euihunbaek@gmail.com>
2024-06-26 19:32:44 +0900

    init

--- src/construct/ConstructMain1.java
+++ src/construct/ConstructMain1.java
@@ -1,6 +1,6 @@
 package construct;
 
-public class ConstructMain1 {
+public class  ConstructMain1 {
     public static void main(String[] args) {
         MemberConstruct member1 = new MemberConstruct("user1",15,90);
         MemberConstruct member2 = new MemberConstruct("user2",15,90);
